c74dc4e063987728913dda675f49027c07b29494
zaidaKary <zaida.260194@gmail.com>
2019-12-23 11:55:48 -0500

    Updates

---
+++ precache-manifest.1cb1cfa3584518f06981721ec6b2b726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c133f7c095e74be082abdd91de43f3b9",
+    "url": "/ProyectoAimoGithub/index.html"
+  },
+  {
+    "revision": "08a5f0ccdc56a4d45553",
+    "url": "/ProyectoAimoGithub/static/css/main.b6641775.chunk.css"
+  },
+  {
+    "revision": "40b7d7a6ea12d97c4451",
+    "url": "/ProyectoAimoGithub/static/js/2.d8ec38fb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ProyectoAimoGithub/static/js/2.d8ec38fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08a5f0ccdc56a4d45553",
+    "url": "/ProyectoAimoGithub/static/js/main.d21e0daa.chunk.js"
+  },
+  {
+    "revision": "cecefefca0bec708cf77",
+    "url": "/ProyectoAimoGithub/static/js/runtime-main.358c0d62.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/ProyectoAimoGithub/static/media/logoGithub.0ba2aa20.png"
+  }
+]);
